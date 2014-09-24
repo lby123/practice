905366e3561bf8ca88cb93c95c55992cf43e05d1
lby123 <lby0219@yeah.net>
2014-09-24 21:05:41 +0800

    guess

---
+++ guess/guess/guess.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	const int max_tries = 3;
+	string name;
+	int usr_val;
+	int num_tries = 0;
+	int num_right = 0;
+	char usr_more;
+	bool go_for_it = true;
+	char usr_rsp;
+	int num_of_tries;
+	bool got_it;
+	int correct_guess;
+	char try_again;
+	bool next_seq;
+
+
+}
